9cc135cf4729eb320291788431e8f650a22872ce
batistahermogenes <hermogenes@simbioseventures.com>
2018-01-30 02:23:23 -0300

    Put a simple example to try to understand bayes law and create a generic formula to it, based on previous formulas created to calculate probabilities.

--- stats/chapter_6/bayes_probabilities.py
+++ stats/chapter_6/bayes_probabilities.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals, division
 import random
+from . import probabilities
 
 
 decease_events = [0 for _ in range(10000)]
@@ -8,6 +9,10 @@ decease_events[-1] = 1
 
 test_events = [0 for _ in range(100)]
 test_events[-1] = 1
+
+
+def bayes_law(p_a, p_b_a, p_b_not_a, not_a):
+    return (p_b_a * p_a)/((p_b_a * p_a) + (p_b_not_a * not_a))
 
 
 if __name__ == "__main__":
@@ -29,3 +34,31 @@ if __name__ == "__main__":
     print("Total deceases with positive test: ", desease_positive_counts)
     print("Total deceases with positive test (%): ", (desease_positive_counts / 1000000) * 100)
     print("Total no deceases with positive test: ", no_decease_positive_counts)
+
+    microwaves_broken = [1, 0, 0, 0]
+    # pa = 0.25
+    really_broken = probabilities.probability([1], microwaves_broken)
+    not_broken = probabilities.oposite_probability([1], microwaves_broken)
+
+    # pba = 0.9
+    possibles_diagnostics = []
+    undefined_diagnostic = [None for _ in range(5)]
+    correct_diagnostic = [1 for _ in range(90)]
+    wrong_diagnostic = [0 for _ in range(5)]
+    possibles_diagnostics.extend(undefined_diagnostic)
+    possibles_diagnostics.extend(correct_diagnostic)
+    possibles_diagnostics.extend(wrong_diagnostic)
+
+    # pba = 0.9
+    # Probability: given microwaves really broken,
+    # the professional give correct diagnostic (microwave broken)
+    pba = probabilities.probability([1], possibles_diagnostics)
+
+    # pbnota = 0.05
+    # Probability: given microwaves did not broken,
+    # the professional give incorrect diagnostic
+    # (tell you that microwave was broken)
+    pbnota = 0.05
+
+    # Expected 85.7 %
+    print(bayes_law(really_broken, pba, pbnota, not_broken))
