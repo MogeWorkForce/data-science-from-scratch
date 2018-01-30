1c6f212023c11968cb78bfd2d0c05923b1b2d931
batistahermogenes <hermogenes@simbioseventures.com>
2018-01-30 00:54:27 -0300

    Add new samples and formulas on file

--- stats/chapter_6/probabilities.py
+++ stats/chapter_6/probabilities.py
@@ -2,7 +2,6 @@
 from __future__ import absolute_import, unicode_literals, division
 
 import random
-import copy
 
 gender = ["boy", "girl"]
 dice_faces = list(range(1, 7))
@@ -27,9 +26,12 @@ def valid_sample(positive_events, type_events):
     return real_events
 
 
-def probability(positive_events, type_events, exclude_n_event=0):
+def probability(positive_events, type_events, exclude_n_event=0,
+                keep_desired_events=False):
     events = valid_sample(positive_events, type_events)
-    desired_events = len(events) - exclude_n_event
+    desired_events = len(events)
+    if not keep_desired_events and exclude_n_event > 0:
+        desired_events = len(events) - exclude_n_event
     total_events_in_analysis = len(type_events) - exclude_n_event
     return desired_events / total_events_in_analysis
 
@@ -43,7 +45,8 @@ def oposite_probability(positive_events, type_events):
 
 
 def probability_of_multiple_events(desired_outcomes, type_events,
-                                   remove_from_events=False):
+                                   remove_from_events=False,
+                                   keep_desired_events=False):
     """
         Simple formula to calculate probability of multiple events.
         When `remove_from_events = True`, it's indicate of that outcome
@@ -51,15 +54,32 @@ def probability_of_multiple_events(desired_outcomes, type_events,
         Ex:
             We have deck of cards (52 cards), and with consecutive draw,
             we will continue to get a next card without put again on deck
-            so the number of possible events will decrease.
+            the previous drawed card, so the number of possible events
+            will decrease.
     """
     result = 1
     exclude_n_events = 0
     for outcome in desired_outcomes:
-        result *= probability(outcome, type_events, exclude_n_events)
+        result *= probability(
+            outcome, type_events, exclude_n_events, keep_desired_events)
         if remove_from_events:
             exclude_n_events += 1
 
+    return result
+
+
+def conditional_probability(outcomes, events_list):
+    """
+        Outcomes must be a list with 2 dimensions
+    """
+    result = 1
+    for i, outcome_event_zipped in enumerate(zip(outcomes, events_list)):
+        outcome, events = outcome_event_zipped
+        current_probability = probability(outcome, events)
+        if i == 0:
+            result *= current_probability
+        else:
+            result /= current_probability
     return result
 
 
@@ -88,7 +108,7 @@ if __name__ == "__main__":
     print("P(both | either):", both_girls / either_girl)
 
     print("-" * 10)
-    desired_outputs = [1]
+    desired_outputs = [1, 4]
     print("Probabilities when roll dice in one event lower or equals than sample:", desired_outputs,
           probability(desired_outputs, dice_faces))
     print("Probabilities when roll dice in one event is not lower or equals than sample:", desired_outputs,
@@ -97,31 +117,78 @@ if __name__ == "__main__":
     print("-" * 10)
     # In first event
     desired_outcomes = [
-        [1, 2], [2, 4, 5]
+        [1], [1]
     ]
     p_dices = probability_of_multiple_events(desired_outcomes, dice_faces)
     print(
-        "Probabilities when first events is equal %r and second %r is: %s"
+        "Probabilities when first events is equal %r and second %r is: %.2f"
         % (
             desired_outcomes[0],
             desired_outcomes[1],
-            p_dices
+            p_dices * 100
         )
     )
 
     print("-" * 10)
 
     desired_outcomes = [
-        [str(i)+card_suit[0] for i in range(1, 11)],
-        [str(i)+card_suit[0] for i in range(1, 11)]
+        [str(i)+card_suit[0] for i in range(1, 14)],
+        [str(i)+card_suit[0] for i in range(1, 14)]
     ]
     p_cards = probability_of_multiple_events(
         desired_outcomes, deck, remove_from_events=True)
     print(
-        "Probabilities when first events is equal %r and second %r is: %s"
+        "Probabilities when first events is equal %r and second %r is: %.2f"
         % (
             desired_outcomes[0],
             desired_outcomes[1],
-            p_cards
+            p_cards * 100
+        )
+    )
+
+    print("-" * 10)
+    print("Multiple alternatives with decrease total events, but changing"
+          " desired events.")
+    blues = ["B" for _ in range(5)]
+    whites = ["W" for _ in range(4)]
+    reds = ["R" for _ in range(11)]
+    all_balls = []
+    all_balls.extend(blues)
+    all_balls.extend(reds)
+    all_balls.extend(whites)
+    desired_outcomes = [
+        blues,
+        whites,
+        reds
+    ]
+
+    p_balls = probability_of_multiple_events(
+        desired_outcomes, all_balls, remove_from_events=True,
+        keep_desired_events=True)
+    print(
+        "Probabilities when %r desired outcomes: %.2f"
+        % (
+            desired_outcomes,
+            p_balls * 100
+        )
+    )
+
+    events_list = [
+        deck,
+        deck
+    ]
+
+    outcomes = [
+        ["1S"],
+        [x for x in deck if "S" in x]
+    ]
+
+    p_spades = conditional_probability(outcomes, events_list)
+
+    print(
+        "Probabilities Conditional when %r desired outcomes: %.2f"
+        % (
+            outcomes,
+            p_spades * 100
         )
     )
